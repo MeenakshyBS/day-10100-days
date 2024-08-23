70a4b90688373bc2c72375dbbbb5cc0801564dbb
Meenakshy Bindu Suresh <meenakshy.b.suresh@gmail.com>
2024-08-23 13:34:01 +0000

    challenge

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 myBill = float(input("What was the bill?: "))
 numberOfPeople = int(input("How many people?: "))
+tip = float(input("what % of tip you want to leave?(15%, 18%, 20%): "))
+tip = tip / 100
+tip = myBill*tip
+myBill = myBill + tip
 answer = myBill / numberOfPeople
 answer = round(answer, 2)
 print("You all owe", answer)
