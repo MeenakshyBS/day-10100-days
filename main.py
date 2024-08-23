14d90ad2e196ea83874491cab0ed1e21f0f608ff
Meenakshy Bindu Suresh <meenakshy.b.suresh@gmail.com>
2024-08-23 13:22:37 +0000

    fmc

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-adding = 4 + 3
-print(adding)
+# 👉 Solve the following problems with my code
+# Your goal is to print the solution of all 3 calculations to the screen.
 
-subtraction = 8 - 9
-print(subtraction)
-
-multiplication = 4 * 32
+# multiplication
+multiplication = 3.4 * 6.8
 print(multiplication)
-
-division = 50 / 5
+# division
+division = 2467 / 4673
 print(division)
-
-# a number raised to the power of some number
-# in this example we raise 5 to the power of 2
-squared = 5**2
-print(squared)
-
-# remainder of a division
-modulo = 15 % 4
-print(modulo)
-
-# whole number division
-divisor = 15 // 2
-print(divisor)
+#raise 10 to the power of 2
+square = 10**2
+print(square)
+# print the remainder when 343 is divided by 4
+remainder = 343 % 4
+print(343 // 100)
