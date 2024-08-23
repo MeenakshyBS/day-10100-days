2e04b3e296fcdf15113d5dfb90da5302b87034d7
Meenakshy Bindu Suresh <meenakshy.b.suresh@gmail.com>
2024-08-23 13:25:53 +0000

    fmc2

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-# 👉 Solve the following problems with my code
-# Your goal is to print the solution of all 3 calculations to the screen.
-
-# multiplication
-multiplication = 3.4 * 6.8
-print(multiplication)
-# division
-division = 2467 / 4673
-print(division)
-#raise 10 to the power of 2
-square = 10**2
-print(square)
-# print the remainder when 343 is divided by 4
-remainder = 343 % 4
-print(343 // 100)
+myBill = float(input("What was the bill?: "))
+numberOfPeople = int(input("How many people?: "))
+answer = myBill / numberOfPeople
+answer = round(answer, 2)
+print("You all owe", answer)
